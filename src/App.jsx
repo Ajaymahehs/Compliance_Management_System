ee063ef514f119611ee6adf7bad4c0509f632cca
Ajaymahesh <ajayalka583446@gmail.com>
2026-07-31 01:21:07 +0530

    "Add About Page"

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
+import About from './component/about';
 import Home from './component/home';
 
 function App() {
   return (
     <div className="App">
       <Home/>
+      <About/>
     </div>
   );
 }
