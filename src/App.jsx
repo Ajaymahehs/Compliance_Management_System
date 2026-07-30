fd07046289ebfe1c93a5d8e0c83db66ab29bf6ae
Ajaymahesh <ajayalka583446@gmail.com>
2026-07-31 01:25:17 +0530

    ADD Products

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import About from './component/about';
 import Home from './component/home';
+import Products from './component/product';
 
 function App() {
   return (
     <div className="App">
       <Home/>
       <About/>
+      <Products/>
     </div>
   );
 }
